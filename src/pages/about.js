887ce064213cef22f66bdf7e77e56ca51f215b32
fedetanco22 <fedetanco84@gmail.com>
2021-11-12 14:40:25 -0300

    int funtionality working

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,30 +1,19 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import { useRouter } from 'next/router';
-
 import {useTranslations} from 'next-intl';
 
-export default function Home() {
-  const t = useTranslations('home');
-  const router = useRouter();
-  console.log(router.locale);
-  console.log(router.pathname);
-  console.log(router);
-  
- 
+const About = () => {
+const t = useTranslations('about');
   return (
     <div>
-      <h1>{t('title')}</h1>
+       <h1>{t('title')}</h1>
      <h1> {t("prueba")}</h1>
      <p>{t("auth.SignUp.title")}</p>
      <p>{t("auth.SignUp.form.placeholder")}</p>
-     <p>{}</p>
     </div>
   )
 }
 
-// pages/index.js
+export default About
+
 export function getStaticProps({locale}) {
   return {
     props: {
