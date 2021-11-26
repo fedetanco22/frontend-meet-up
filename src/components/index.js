e3b0c8ba801630892fa7b4d0e7ad1110c48d432a
darioviada <darioviada@gmail.com>
2021-11-26 08:52:10 -0300

    se agrega layout panel

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,7 @@
 import Layout from "./Layout/Layout";
 import Navbar from "./Navbar/Navbar";
+import LayoutPanel from "./LayoutPanel/LayoutPanel";
+import NavbarPanel from "./NavbarPanel/NavbarPanel";
 import SEO from "./SEO/SEO";
 import Banner from "./Banner/Banner";
 
@@ -7,6 +9,8 @@ import Banner from "./Banner/Banner";
 export{
   Navbar, 
   Layout,
+  NavbarPanel, 
+  LayoutPanel,
   SEO,
   Banner,
 }
