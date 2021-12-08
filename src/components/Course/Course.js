647c78ef3f7e59526c0939ea417df41cff383ab9
fedetanco22 <fedetanco84@gmail.com>
2021-12-08 11:30:20 -0300

    coursesList & course

--- src/components/Course/Course.js
+++ src/components/Course/Course.js
@@ -1,19 +1,35 @@
 import Image from 'next/image'
+import {Button} from '../index';
+import styles from './Course.module.scss';
+
 const Course = ({course}) => {
   const {id, title, description, url, image} = course;
   return (
-    <div className="col col-md-3" >
-      <div style={{width:'100%'}}>
-        <Image src={image} alt="" 
-          width={700}
-          height={400}
-          objectFit="cover"
-          quality={100}
-        />
+    <div className='col-12 col-sm-6 col-md-4'>
+      <div className={`${styles.course}  `} >
+        <div style={{width:'100%'}}>
+          <Image src={image} alt="" 
+            width={580}
+            height={377}
+            objectFit="cover"
+            quality={100}
+          />
+        </div>
+        <div className={styles.text}>
+          <h6>for students</h6>
+          <h5 className={styles.text__title}>{title}</h5>
+          <p className={styles.text__description}>{description}</p>
+        </div>
+       <div className='text-end'>
+        <Button
+          text='Enroll' 
+          linkAsButton
+          path={'/enroll'}
+          buttonType='blue_small'
+          />
+       </div>
+        <a href={url}>{url}</a>
       </div>
-      <h4>{title}</h4>
-      <p>{description}</p>
-      <a href={url}>{url}</a>
     </div>
   )
 }
